3aadba96d3f7d4c6bcd438d19dd05f48020a89fd
y_neline <aliya.arapbayeva@gmail.com>
2024-01-23 20:10:27 +0600

    Initial commit

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+export const todo = () => {
+  return (
+    <div className='Todo'>
+      <p>Go to school</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
+    </div>
+  )
+}
